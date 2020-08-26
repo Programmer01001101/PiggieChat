5a586309ca40ad978c28dcb094cd64a706dc04f4
Ruixiang Qi <ruixiang.qi@guarankey.com>
2020-08-26 19:57:56 -0400

    Initial Commit

---
+++ piggiechat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  piggiechat
+//
+//  Created by Microapples on 8/26/20.
+//  Copyright © 2020 Programmer7. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
